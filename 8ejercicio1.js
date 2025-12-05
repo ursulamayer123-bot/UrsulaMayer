0174ebcee3d418686a8a82f1fc9adc7ce5c217e2
Ursula Mayer <ursulamayer123@gmail.com>
2025-12-05 17:00:44 -0300

    Clase8

---
+++ 8ejercicio1.js
@@ -0,0 +1,8 @@
+const prompt = require ('prompt-sync')();
+
+let numero = parseFloat (prompt('Ingrese su número: '));
+
+let dobleNumero = numero * 2;
+
+console.log('El doble de tu número ingresado es:' + dobleNumero);
+
